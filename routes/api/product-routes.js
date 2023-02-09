2d3a8317ba3ece43b86df7262bf14881b6d1370f
Junho Kim <kjh01222@gmail.com>
2023-02-08 20:46:50 -0500

    CRUD for tag, products, and categories added

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,45 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [Category, {
+        model:Tag,
+        through: ProductTag
+      }]
+    });
+    res.status(200).json(productData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+        include: [Category, {
+        model:Tag,
+        through: ProductTag
+      }]
+    });
+    if(!productData) {
+      res.status(404).json({message: "There is no product found withthin the given id."})
+      return;
+    }
+    res.status(200).json(productData)
+  } catch (err){
+    res.status(500).json(err);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -88,9 +114,21 @@ router.put('/:id', (req, res) => {
       res.status(400).json(err);
     });
 });
-
-router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!productData) {
+      res.status(404).json({message: "No product found with given id"})
+    }
+    res.status(200).json(productData);
+  } catch(err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
